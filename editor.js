86c2517950a6343eabd9b427c6def9a3117aa117
dimitrile <lebedim@yahoo.gr>
2018-06-16 16:05:20 +0300

    Update editor.js

--- editor.js
+++ editor.js
@@ -25,7 +25,7 @@ function init() {
     clickable: true
   });
 	
-    map.data.loadGeoJson("data/2013080_review.geojson");
+    map.data.loadGeoJson("data/2013080.geojson");
 
     map.data.setStyle(function(feature) {
         var avg = get_avg(feature.getProperty('ratings'));
